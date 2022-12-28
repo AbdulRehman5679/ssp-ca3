53ec7dc4f2fac7319e5a94f20a7bef2ecac8ba0f
Mani8588 <moonrana173@yahoo.com>
2022-12-28 23:39:08 +0000

    table configure

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const   http = require('http'), //HTTP server
-        path = require('path'),
-        express = require('express'), //Handling HTTP requests & routing
-        fs = require('fs'), //File system functionalities
-        xmlParse = require('xslt-processor').xmlParse, //XML handling
-        xsltProcess = require('xslt-processor').xsltProcess, //XSLT handling
-        router = express(), //Init our router
-        xml2js = require('xml2js'),
-        server = http.createServer(router); //Init our server
-        
-        router.use(express.static(path.resolve(__dirname,'views')));
-        router.use(express.urlencoded({extended: true}));
-        router.use(express.json());
+const http = require('http'), //HTTP server
+    path = require('path'),
+    express = require('express'), //Handling HTTP requests & routing
+    fs = require('fs'), //File system functionalities
+    xmlParse = require('xslt-processor').xmlParse, //XML handling
+    xsltProcess = require('xslt-processor').xsltProcess, //XSLT handling
+    router = express(), //Init our router
+    xml2js = require('xml2js'),
+    server = http.createServer(router); //Init our server
 
-function XMLtoJSON(filename, cb){
+router.use(express.static(path.resolve(__dirname, 'views')));
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+function XMLtoJSON(filename, cb) {
     let filepath = path.normalize(path.join(__dirname, filename));
-    fs.readFile(filepath, 'utf8', function(err, xmlStr){
+    fs.readFile(filepath, 'utf8', function (err, xmlStr) {
         if (err) throw (err);
         xml2js.parseString(xmlStr, {}, cb);
     });
 };
 
-function JSONtoXML(filename, obj, cb){
+function JSONtoXML(filename, obj, cb) {
     let filepath = path.normalize(path.join(__dirname, filename));
     let builder = new xml2js.Builder();
     let xml = builder.buildObject(obj);
@@ -28,9 +28,9 @@ function JSONtoXML(filename, obj, cb){
     fs.writeFile(filepath, xml, cb);
 };
 
-router.get('/get/html', function(req, res) {
+router.get('/get/html', function (req, res) {
 
-    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
     let xml = fs.readFileSync('menu.xml', 'utf8'),
         xsl = fs.readFileSync('menu.xsl', 'utf8');
@@ -43,14 +43,14 @@ router.get('/get/html', function(req, res) {
     res.end(html.toString());
 });
 
-router.post('/post/json', function(req, res){
-    function appendJSON(obj){
+router.post('/post/json', function (req, res) {
+    function appendJSON(obj) {
         console.log(obj);
-        XMLtoJSON('menu.xml', function(err, result) {
+        XMLtoJSON('menu.xml', function (err, result) {
             if (err) throw (err);
-            result.menu.category[obj.sec_n].item.push({'listing': obj.listing, 'price': obj.price});
+            result.menu.category[obj.sec_n].item.push({ 'listing': obj.listing, 'price': obj.price });
             console.log(JSON.stringify(result, null, " "));
-            JSONtoXML('menu.xml', result, function(err){
+            JSONtoXML('menu.xml', result, function (err) {
                 if (err) console.log(err);
             });
         });
@@ -61,15 +61,15 @@ router.post('/post/json', function(req, res){
     res.redirect('back');
 });
 
-router.post('/post/delete', function (req,res) {
+router.post('/post/delete', function (req, res) {
     function deleteJSON(obj) {
         console.log(obj);
-        XMLtoJSON('menu.xml', function(err, result){
+        XMLtoJSON('menu.xml', function (err, result) {
             if (err) throw (err);
 
             delete result.menu.category[obj.section].item[obj.entree];
 
-            JSONtoXML('menu.xml', result, function(err){
+            JSONtoXML('menu.xml', result, function (err) {
                 if (err) console.log(err);
             });
         });
@@ -79,7 +79,8 @@ router.post('/post/delete', function (req,res) {
 
     res.redirect('back');
 })
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
     const addr = server.address();
     console.log("Server listening at", addr.address + ":" + addr.port)
 });
